494cc0ecfef2a6080a7f69856e13c3171a814e11
markodan <markodan@telekom.rs>
2024-03-15 13:20:02 +0100

    Initial commit

---
+++ src/test/java/com/onboarding/movies/moviesws/MoviesWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onboarding.movies.moviesws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
